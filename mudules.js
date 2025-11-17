4d80bbcf6d6157b40b7c45ead3be38d0bacc1edd
Christian Barbadillo Silva <102840367+cHrisAnCk@users.noreply.github.com>
2025-11-16 21:33:49 -0500

    Add files via upload

---
+++ mudules.js
@@ -0,0 +1,34 @@
+// modules.js - Lista de todos los sistemas
+const MODULES = {
+  generales: [
+    { id: "sigea", name: "SIGEA", icon: "iconos/EG/INICIO/iconSIGEA.svg", url: "sistemas/sigea.html" },
+    { id: "sisloc", name: "SISLOC", icon: "iconos/EG/INICIO/iconSIGLOC.svg", url: "sistemas/sisloc.html" },
+    { id: "sasa", name: "SASA", icon: "iconos/EG/INICIO/iconSASA.svg", url: "sistemas/sasa.html" },
+    { id: "etlv", name: "ETLV", icon: "iconos/EG/INICIO/iconETLV.svg", url: "sistemas/etlv.html" },
+    { id: "side", name: "SIDE", icon: "iconos/EG/INICIO/iconSIDE.svg", url: "sistemas/side.html" },
+    { id: "sidemm", name: "SIDEMM", icon: "iconos/EG/INICIO/iconSIDEMM.svg", url: "sistemas/sidemm.html" },
+    { id: "clv", name: "CLV", icon: "iconos/EG/INICIO/iconCLV.svg", url: "sistemas/clv.html" },
+    { id: "sigap", name: "SIGAP", icon: "iconos/EG/INICIO/iconSIGAP.svg", url: "sistemas/sigap.html" },
+    { id: "sce", name: "SCE", icon: "iconos/EG/INICIO/iconSCE.svg", url: "sistemas/sce.html" },
+    { id: "claridad", name: "CLARIDAD", icon: "iconos/EG/INICIO/iconCLARIDAD.svg", url: "sistemas/claridad.html" },
+    { id: "cae", name: "CAE", icon: "iconos/EG/INICIO/iconCAE.svg", url: "sistemas/cae.html" },
+    { id: "cemm", name: "CEMM", icon: "iconos/EG/INICIO/iconCEMM.svg", url: "sistemas/cemm.html" },
+    { id: "gemma", name: "GEMMA", icon: "iconos/EG/INICIO/iconGEMMA.svg", url: "sistemas/gemma.html" },
+    { id: "ropc", name: "ROPC", icon: "iconos/EG/INICIO/iconROPC.svg", url: "sistemas/ropc.html" },
+    { id: "sam", name: "SAM", icon: "iconos/EG/INICIO/iconSAM.svg", url: "sistemas/sam.html" },
+    { id: "scm", name: "SCM", icon: "iconos/EG/INICIO/iconSCM.svg", url: "sistemas/scm.html" },
+    { id: "sgcae", name: "SGCAE", icon: "iconos/EG/INICIO/iconSGCAE.svg", url: "sistemas/sgcae.html" },
+    { id: "sisgoe", name: "SISGOE", icon: "iconos/EG/INICIO/iconSISGOE.svg", url: "sistemas/sisgoe.html" },
+    { id: "sisplan", name: "SISPLAN", icon: "iconos/EG/INICIO/iconSISPLAN.svg", url: "sistemas/sisplan.html" }
+  ],
+  primarias: [
+    { id: "sigea", name: "SIGEA", icon: "iconos/EG/INICIO/iconSIGEA.svg", url: "sistemas/sigea.html" },
+    { id: "sisloc", name: "SISLOC", icon: "iconos/EP/INICIO/iconSIGLOC.svg", url: "sistemas/sisloc.html" },
+    { id: "onpeduca", name: "ONPEDUCA CPO", icon: "iconos/EP/INICIO/iconONPEDUCA.svg", url: "sistemas/onpeduca.html" },
+    { id: "rep", name: "REP", icon: "iconos/EP/INICIO/iconREP.svg", url: "sistemas/rep.html" },
+    { id: "sciap", name: "SCIAP", icon: "iconos/EP/INICIO/iconSCIAP.svg", url: "sistemas/sciap.html" },
+    { id: "clv", name: "CLV", icon: "iconos/EP/INICIO/iconCLV.svg", url: "sistemas/clv.html" },
+    { id: "claridad", name: "CLARIDAD", icon: "iconos/EP/INICIO/iconCLARIDAD.svg", url: "sistemas/claridad.html" },
+    { id: "sigma", name: "SIGMA", icon: "iconos/EP/INICIO/iconSIGMA.svg", url: "sistemas/sigma.html" }
+  ]
+};
